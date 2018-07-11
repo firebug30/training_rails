2e580da43d7e24b29d20ff7f0080bfad385bd55e
浜野秋仁 <s.sakae3354@gmail.com>
2018-07-11 15:36:48 +0900

    Edit devise and comment

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,13 +1,21 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  before_action :authenticate_user!
   before_action :configure_permitted_parameters, if: :devise_controller?
-  before_action :layout, if: :devise_controller?
+
+  def layout
+    render layout: 'web/layouts/base'
+  end
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname])
   end
 
-  def layout
-    render layout: 'web/layouts/base'
+  def after_sign_in_path_for(resource)
+    root_path
+  end
+
+  def after_sign_out_path_for(resource)
+    root_path
   end
 end
---
+++ db/migrate/20180711045158_add_nickname_column_to_users.rb
@@ -0,0 +1,5 @@
+class AddNicknameColumnToUsers < ActiveRecord::Migration[5.1]
+  def change
+    add_column :users, :nickname, :string
+  end
+end
